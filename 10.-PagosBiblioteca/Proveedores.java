58753bb0ee43de50a7fcf3c88295fba8618fc29f
demonassit <jminorg@ipn.mx>
2021-11-24 10:15:28 -0600

    Regalo de examen n_n

--- 10.-PagosBiblioteca/Proveedores.java
+++ 10.-PagosBiblioteca/Proveedores.java
@@ -1,4 +1,5 @@
 /*
+1- 20
 
 Suma de los cotos de la biblioteca
 Costo mayor
@@ -7,3 +8,82 @@ costro promedio
 costo total de los productos (libros)
 
 */
+
+
+import java.util.*;
+import javax.swing.*;
+import java.lang.Math;
+
+
+public class Proveedores(){
+
+    private String nombre_cliente;
+    private String producto[] = new String[10];
+    private int costo[] = new int[10];
+    private int producto_existente[] = new int[10];
+
+    //los metodos Getter Y Setter de cada variable
+
+
+    //menu
+
+    public void MenuProveedores(){
+
+        String text;
+        //try catch para el manejo de errores
+        // un ciclo do while para repetir las opciones
+        text = JOptionPane.showInputDialog("Seleccione la funcion deseada: "
+                                            + "\n 1.- Suma total de costos"
+                                            + "\n 2.- Costo Mayor"
+                                            + "\n 3.- Costo Menor"
+                                            + "\n 4.- Costo Promedio"
+                                            + "\n 5.- Costo Total del Producto");
+                                            
+        /*
+        
+        De los libros que se crearon deberan de agregar la clase costo aplicando herencia
+        para que se pueda manejar el costo de cada libro
+
+        Libro drama = new Libro("La cumbre escarlata", "No me lo se", "12345-BN", 5, 44);
+        Libro fantasmas = new Libro("Las cronicas Mutantes", "No me lo se", "00345-BN", 3, 608);
+
+        Si elijo la opcion 1 total del costo de la cumbre escarlata : 5*44
+
+        Si elijo la opcion 2 se debe de obtener cual es el libro que tiene mayor costo: Las cronicas mutantes con 608
+
+        Si eljijo la opcion 3 debe de salir el libro mas barato : la cumbre escarlata 44
+
+        Si elijo la opcion 4 se debe de sacar el promedio de los costos de los libros 
+
+        Si eljjo la opcion 5 : deben de multiplicar cantidad de libros los ejemplares * costo de cada uno 
+        
+        */
+
+        //aqui hay que agregar el menu de las opciones
+        int opcion;
+
+        switch (opcion) {
+            case 1:
+                SumaTotal();
+                break;
+            case 2:
+                CostoMayor();
+                break;
+            case 3:
+                CostoMenor();
+                break;
+            case 4:
+                CostoPromedio();
+                break;
+                CostoTotal();
+            case 5:
+                
+                break;
+        
+            default:
+                //un mensajito de gracias :3
+                break;
+        }
+    }
+ 
+}
--- 10.-PagosBiblioteca/sueldoEmpleado.java
+++ 10.-PagosBiblioteca/sueldoEmpleado.java
@@ -1,4 +1,6 @@
 /*
+21 - 40
+
 Obtener los pagos correspondientes a los trabajadores
 
 ingresar el nombre del trabajador
@@ -6,3 +8,52 @@ sueldo
 tipo de trabajador
 
 */ 
+
+
+import javax.swing.*;
+import java.util.*;
+
+
+public class sueldoEmpleado(){
+
+    private int ID_empleado, num_horas;
+    private String nombre, appat, appatmat;
+    private Double sueldo;
+
+
+    public void MenuEmpleado(){
+
+        String text;
+
+        text = JOptionPane.showInputDialog("Selecciona la opcion deseada:"
+                                        + "\n 1.- Agregar nuevo empleado"
+                                        + "\n 2.- Consultar empleado por ID"
+                                        + "\n 3.- Consultar todos los empleados"
+                                        + "\n 4.- Pagar al empleado por ID");
+
+        /*
+        
+        Primero debo de tener una clase llamada Empleado
+
+        Quiero 5 Empleados 
+
+        Si elijo 1 
+        Se debe de ingresar id, num_horas que trabaja, nombre, appat, appmat, sueldo por hr
+
+        Si elijo 2
+        Se debe de tener un metodo en el cual se va a buscar por el ID
+
+        public int buscaridempleado(int id)
+
+        Si elijo 3
+        Debo de imprimir la informacion de todos los empleados
+
+        Si elijo 4
+        VOy a tener dos opciones
+        1 pago quincenal
+            sueldo y el num de horas * 15 y que aparezca la informacion del empleado + el pago
+        2 pago mensual
+            sueldo y el num de horas * 30 y que aparezca la informacion del empleado + el pago
+        */
+    }
+}
