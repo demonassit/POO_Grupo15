76bdb1290c8c8962edf9d9d5219af738e0b12df0
demonassit <jminorg@ipn.mx>
2022-02-08 07:58:02 -0600

    formularios final

--- 23.-Formularios/js/validarformulario.js
+++ 23.-Formularios/js/validarformulario.js
@@ -30,4 +30,47 @@ function validar(formulario){
         formulario.nombre.focus();
         return (false);
     }
+
+    if(formulario.edad.value.length > 2){
+        alert("Escriba solo 2 digitos en el campo edad");
+        formulario.nombre.focus();
+        return false;
+    }
+
+
+    var checkOk = "0987654321";
+    var checkStr = formulario.edad.value;
+    var allvalid = true;
+
+    for(var i = 0; i < checkStr.length; i++ ){
+        var ch = checkStr.charAt(i);
+             
+          for(var j = 0; j < checkOk.length; j++){
+            if(ch == checkOk.charAt(j)) 
+          
+            break;
+        }
+            if(j == checkOk.length){
+                allvalid = false;
+                break;
+            }
+        
+    }
+    if(!allvalid){
+        alert("Escribe solo numeros en el campo Edad");
+        formulario.edad.focus();
+        return (false);
+    }
+
+    //manejo de expresiones regulares
+
+    var txt = formulario.correo.value;
+
+    var b = /^[^@\s]+@[^@\.\s]+(\.[^@\.\s]+)$/;
+
+    alert("Email " + (b.test(txt)?"":" no ")+"valido");
+
+    return b.test(txt);
+
+
 }
